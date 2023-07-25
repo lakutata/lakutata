86b397f8d7c83b5c141eba713c90e4e177ce06e7
alex <myq1991@gmail.com>
2023-07-25 20:29:42 +0800

    update

--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -1,6 +1,8 @@
 import {Application} from '../Application.js'
-
+// import {Validator,StringSchema,NumberSchema,ObjectSchema} from '../../build/lib/Validator.js'
 
 (async () => {
     await new Application().test()
+
+    // const sch = Validator.Object().concat()
 })()
