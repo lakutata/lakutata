404b29e0c36b6649fcd3fd66790237be9e9054cc
alex <myq1991@gmail.com>
2023-08-04 01:44:30 +0800

    update

--- src/Core.ts
+++ src/Core.ts
@@ -5,6 +5,8 @@ export {Application} from './lib/Application.js'
 
 //参数选项
 export {ApplicationOptions} from './options/ApplicationOptions.js'
+export {LoadEntryCommonOptions} from './options/LoadEntryCommonOptions.js'
+export {LoadEntryClassOptions} from './options/LoadEntryClassOptions.js'
 
 //接口
 export {IConstructor} from './interfaces/IConstructor.js'
@@ -16,4 +18,8 @@ export {ISortArrayOptions} from './interfaces/ISortArrayOptions.js'
 
 //基础类
 export {DTO} from './lib/base/DTO.js'
+export {BaseObject} from './lib/base/BaseObject.js'
+export {Component} from './lib/base/Component.js'
+export {Container} from './lib/base/Container.js'
 export {Exception} from './lib/base/abstracts/Exception.js'
+export {Interval} from './lib/base/abstracts/Interval.js'
--- src/Exceptions.ts
+++ src/Exceptions.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata'
 
+export {DependencyInjectionException} from './exceptions/DependencyInjectionException.js'
+export {InvalidConfigurableValueException} from './exceptions/InvalidConfigurableValueException.js'
 export {InvalidDTOValueException} from './exceptions/InvalidDTOValueException.js'
+export {InvalidGlobStringException} from './exceptions/InvalidGlobStringException.js'
 export {InvalidMethodAcceptException} from './exceptions/InvalidMethodAcceptException.js'
 export {InvalidMethodReturnException} from './exceptions/InvalidMethodReturnException.js'
-export {DependencyInjectionException} from './exceptions/DependencyInjectionException.js'
-export {InvalidGlobStringException} from './exceptions/InvalidGlobStringException.js'
+export {InvalidValueException} from './exceptions/InvalidValueException.js'
+export {MethodNotFoundException} from './exceptions/MethodNotFoundException.js'
+
