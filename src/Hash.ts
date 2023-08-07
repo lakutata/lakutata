6bba073c24995b5666700724939ef60373e6958f
alex <myq1991@gmail.com>
2023-08-08 03:28:35 +0800

    update

--- src/Hash.ts
+++ src/Hash.ts
@@ -1,13 +1,38 @@
 import 'reflect-metadata'
 import CryptoJs from 'crypto-js'
-import {createHash} from 'crypto'
+import {createHash, Hash} from 'crypto'
+import {ConvertToStream} from './Utilities.js'
+
 /**
- * MD5哈希算法
+ * 异步哈希
+ * @param algorithm
+ * @param message
+ */
+function asyncHash(algorithm: string, message: string): Promise<string> {
+    return new Promise<string>((resolve, reject): void => {
+        const hash: Hash = createHash(algorithm)
+        ConvertToStream(message)
+            .on('data', chunk => hash.update(chunk))
+            .once('error', reject)
+            .once('end', () => resolve(hash.digest().toString('hex')))
+    })
+}
+
+/**
+ * 异步MD5哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function MD5(message: string, async: true): Promise<string>
+/**
+ * 同步MD5哈希算法
  * @param message
  * @constructor
  */
-export function MD5(message: string): string {
-    return CryptoJs.MD5(message).toString()
+export function MD5(message: string): string
+export function MD5(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('MD5', message) : CryptoJs.MD5(message).toString()
 }
 
 /**
@@ -19,12 +44,20 @@ export function HmacMD5(message: string, key: string): string {
 }
 
 /**
- * SHA1哈希算法
+ * 异步SHA1哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA1(message: string, async: true): Promise<string>
+/**
+ * 同步SHA1哈希算法
  * @param message
  * @constructor
  */
-export function SHA1(message: string): string {
-    return CryptoJs.SHA1(message).toString()
+export function SHA1(message: string): string
+export function SHA1(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA1', message) : CryptoJs.SHA1(message).toString()
 }
 
 /**
@@ -38,12 +71,20 @@ export function HmacSHA1(message: string, key: string): string {
 }
 
 /**
- * SHA256哈希算法
+ * 异步SHA256哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA256(message: string, async: true): Promise<string>
+/**
+ * 同步SHA256哈希算法
  * @param message
  * @constructor
  */
-export function SHA256(message: string): string {
-    return CryptoJs.SHA256(message).toString()
+export function SHA256(message: string): string
+export function SHA256(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA256', message) : CryptoJs.SHA256(message).toString()
 }
 
 /**
@@ -57,12 +98,20 @@ export function HmacSHA256(message: string, key: string): string {
 }
 
 /**
- * SHA224哈希算法
+ * 异步SHA224哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA224(message: string, async: true): Promise<string>
+/**
+ * 同步SHA224哈希算法
  * @param message
  * @constructor
  */
-export function SHA224(message: string): string {
-    return CryptoJs.SHA224(message).toString()
+export function SHA224(message: string): string
+export function SHA224(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA224', message) : CryptoJs.SHA224(message).toString()
 }
 
 /**
@@ -76,12 +125,20 @@ export function HmacSHA224(message: string, key: string): string {
 }
 
 /**
- * SHA512哈希算法
+ * 异步SHA512哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA512(message: string, async: true): Promise<string>
+/**
+ * 同步SHA512哈希算法
  * @param message
  * @constructor
  */
-export function SHA512(message: string): string {
-    return CryptoJs.SHA512(message).toString()
+export function SHA512(message: string): string
+export function SHA512(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA512', message) : CryptoJs.SHA512(message).toString()
 }
 
 /**
@@ -95,12 +152,20 @@ export function HmacSHA512(message: string, key: string): string {
 }
 
 /**
- * SHA384哈希算法
+ * 异步SHA384哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA384(message: string, async: true): Promise<string>
+/**
+ * 同步SHA384哈希算法
  * @param message
  * @constructor
  */
-export function SHA384(message: string): string {
-    return CryptoJs.SHA384(message).toString()
+export function SHA384(message: string): string
+export function SHA384(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA384', message) : CryptoJs.SHA384(message).toString()
 }
 
 /**
@@ -114,12 +179,20 @@ export function HmacSHA384(message: string, key: string): string {
 }
 
 /**
- * SHA3哈希算法
+ * 异步SHA3哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function SHA3(message: string, async: true): Promise<string>
+/**
+ * 同步SHA3哈希算法
  * @param message
  * @constructor
  */
-export function SHA3(message: string): string {
-    return CryptoJs.SHA3(message).toString()
+export function SHA3(message: string): string
+export function SHA3(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('SHA3', message) : CryptoJs.SHA3(message).toString()
 }
 
 /**
@@ -133,10 +206,18 @@ export function HmacSHA3(message: string, key: string): string {
 }
 
 /**
- * RIPEMD160哈希算法
+ * 异步RIPEMD160哈希算法
+ * @param message
+ * @param async
+ * @constructor
+ */
+export async function RIPEMD160(message: string, async: true): Promise<string>
+/**
+ * 同步RIPEMD160哈希算法
  * @param message
  * @constructor
  */
-export function RIPEMD160(message: string): string {
-    return CryptoJs.RIPEMD160(message).toString()
+export function RIPEMD160(message: string): string
+export function RIPEMD160(message: string, async?: boolean): string | Promise<string> {
+    return async ? asyncHash('RIPEMD160', message) : CryptoJs.RIPEMD160(message).toString()
 }
--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -5,9 +5,37 @@ import {TestComponent} from './components/TestComponent.js'
 import {TestObject} from './objects/TestObject.js'
 import {TestInterval} from './intervals/TestInterval.js'
 import {TestModule1} from './modules/TestModule1/TestModule1.js'
+import {ConvertToStream} from '../Utilities.js'
+import {createHash, createHmac} from 'crypto'
+import {MD5, RIPEMD160} from '../Hash.js'
 
 (async () => {
 
+    const str: string = 'this is a test'
+    const key: string = 'key123456'
+
+    // const hmac=createHmac('md5',key)
+    // hmac.on('readable', ()=>{
+    //     const data=hmac.read()
+    //     if(data){
+    //         console.log(data.toString('hex'))
+    //     }
+    // })
+    // hmac.write(str)
+    //
+    // hmac.end()
+    // console.log(hmac.digest().toString('hex'))
+
+
+    // const hash=createHash('RIPEMD160')
+    //
+    // ConvertToStream(str).on('data', data=>{
+    //     hash.update(data)
+    // }).on('end', ()=>{
+    //     console.log(MD5(str))
+    //     console.log(hash.digest().toString('hex'))
+    // })
+    return
     const app = await Application.run({
         id: 'test',
         name: 'test',
