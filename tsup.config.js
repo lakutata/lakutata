eb6ed109eeb0753fb459f37fd6528c500e0602d1
myq1991 <myq1991@gmail.com>
2023-08-15 20:40:22 +0800

    Remove banner and update package version to 0.0.3-alpha.6.

--- tsup.config.js
+++ tsup.config.js
@@ -29,16 +29,16 @@ export default defineConfig((options) => {
         terserOptions: {
             mangle: false,
             compress: true
-        },
-        banner: {
-            js: `
-    import path from 'path';
-    import { fileURLToPath } from 'url';
-    import { createRequire as topLevelCreateRequire } from 'module';
-    const require = topLevelCreateRequire(import.meta.url);
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    `
         }
+    //     banner: {
+    //         js: `
+    // import path from 'path';
+    // import { fileURLToPath } from 'url';
+    // import { createRequire as topLevelCreateRequire } from 'module';
+    // const require = topLevelCreateRequire(import.meta.url);
+    // const __filename = fileURLToPath(import.meta.url);
+    // const __dirname = path.dirname(__filename);
+    // `
+    //     }
     }
 })
