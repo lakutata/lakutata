0dfde05c34297ee52dad91d2e0c535a6b8ceb7bb
myq1991 <myq1991@gmail.com>
2023-08-16 18:51:22 +0800

    chore: release v0.0.4-alpha.12

--- src/lib/DefaultLoggerProvider.ts
+++ src/lib/DefaultLoggerProvider.ts
@@ -1,9 +1,9 @@
 import {ILogger} from '../interfaces/ILogger.js'
 import {pino} from 'pino'
-import pretty from 'pino-pretty'
+import pretty from 'pino-pretty/index.js'
 
 export function DefaultLoggerProvider(level: string = 'trace'): ILogger {
     const appName: string = process.env.appName ? process.env.appName : 'Unnamed'
-
-    return pino({level: level, name: appName}, pretty.PinoPretty())
+    const stream = (pretty as any)()
+    return pino({level: level, name: appName}, stream)
 }
