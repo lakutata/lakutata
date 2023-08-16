e51a20a3c62e2f85fe07b357a7f5479083a3e8e2
myq1991 <myq1991@gmail.com>
2023-08-16 23:19:55 +0800

    Remove unnecessary files from the repository.

--- .prepublish.js
+++ .prepublish.js
@@ -8,5 +8,8 @@ await writeFile(backup, JSON.stringify(originPkgJson), {encoding: 'utf-8'})
 
 delete originPkgJson['release-it']
 delete originPkgJson['devDependencies']
-
+const scriptNames = Object.keys(originPkgJson.scripts)
+for (const scriptName of scriptNames) {
+    if (!/publish/.test(scriptName)) delete originPkgJson.scripts[scriptName]
+}
 await writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
