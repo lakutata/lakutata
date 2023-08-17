198326b6ec144b82f94a531a42dad7bed37746c9
myq1991 <myq1991@gmail.com>
2023-08-17 09:58:53 +0800

    chore: release v0.0.4-alpha.39

--- .postpublish.js
+++ .postpublish.js
@@ -1,8 +1,9 @@
 import {readFile, writeFile, rm} from 'fs/promises'
+import {EOL} from 'os'
 
 const target = 'package.json'
 const backup = '.package.json.bak'
 
 const originPkgJson = JSON.parse(await readFile(backup, {encoding: 'utf-8'}))
-await writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
+await writeFile(target, `${JSON.stringify(originPkgJson, null, 2)}${EOL}`, {encoding: 'utf-8', flag: 'w+'})
 await rm(backup, {force: true, recursive: true})
