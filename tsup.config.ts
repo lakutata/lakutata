3d75461af9834062788a8b205b4f6e7130a841d6
myq1991 <myq1991@gmail.com>
2023-08-17 09:31:57 +0800

    chore: release v0.0.4-alpha.37

--- tsup.config.ts
+++ tsup.config.ts
@@ -24,14 +24,15 @@ export default defineConfig(() => {
         keepNames: true,
         noExternal: [/./],
         external: [/@types/, 'tslib'],
-        minify: 'terser',
+        minify: false,
+        // minify: 'terser',
         terserOptions: {
             keep_classnames: true,
             mangle: false,
             compress: true
         },
-        banner: {
-            js: 'const require = await import(\'module\').then($=>$.createRequire(import.meta.url))'
-        }
+        // banner: {
+        //     js: 'const require = await import(\'module\').then($=>$.createRequire(import.meta.url))'
+        // }
     }
 })
