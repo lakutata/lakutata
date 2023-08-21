b5f670e3fba341ae5ac2da6f6eb1c3a8eb884b4f
myq1991 <myq1991@gmail.com>
2023-08-21 15:41:23 +0800

    chore: release v0.1.0

--- .postpublish.js
+++ .postpublish.js
@@ -1,9 +1,11 @@
-import {readFile, writeFile, rm} from 'fs/promises'
-import {EOL} from 'os'
+(async () => {
+    const {readFile, writeFile, rm} = require('fs/promises')
+    const {EOL} = require('os')
 
-const target = 'package.json'
-const backup = '.package.json.bak'
+    const target = 'package.json'
+    const backup = '.package.json.bak'
 
-const originPkgJson = JSON.parse(await readFile(backup, {encoding: 'utf-8'}))
-await writeFile(target, `${JSON.stringify(originPkgJson, null, 2)}${EOL}`, {encoding: 'utf-8', flag: 'w+'})
-await rm(backup, {force: true, recursive: true})
+    const originPkgJson = JSON.parse(await readFile(backup, {encoding: 'utf-8'}))
+    await writeFile(target, `${JSON.stringify(originPkgJson, null, 2)}${EOL}`, {encoding: 'utf-8', flag: 'w+'})
+    await rm(backup, {force: true, recursive: true})
+})()
--- .prepublish.js
+++ .prepublish.js
@@ -1,15 +1,17 @@
-import {readFile, writeFile} from 'fs/promises'
+(async () => {
+    const {readFile, writeFile} = require('fs/promises')
 
-const target = 'package.json'
-const backup = '.package.json.bak'
+    const target = 'package.json'
+    const backup = '.package.json.bak'
 
-const originPkgJson = JSON.parse(await readFile(target, {encoding: 'utf-8'}))
-await writeFile(backup, JSON.stringify(originPkgJson), {encoding: 'utf-8'})
+    const originPkgJson = JSON.parse(await readFile(target, {encoding: 'utf-8'}))
+    await writeFile(backup, JSON.stringify(originPkgJson), {encoding: 'utf-8'})
 
-delete originPkgJson['release-it']
-delete originPkgJson['devDependencies']
-const scriptNames = Object.keys(originPkgJson.scripts)
-for (const scriptName of scriptNames) {
-    if (/release|test/.test(scriptName)) delete originPkgJson.scripts[scriptName]
-}
-await writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
+    delete originPkgJson['release-it']
+    delete originPkgJson['devDependencies']
+    const scriptNames = Object.keys(originPkgJson.scripts)
+    for (const scriptName of scriptNames) {
+        if (/release|test/.test(scriptName)) delete originPkgJson.scripts[scriptName]
+    }
+    await writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
+})()
