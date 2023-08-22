4412322c422ca2941c07fb9df1d0ee28471e642d
myq1991 <myq1991@gmail.com>
2023-08-22 22:43:52 +0800

    Refactor Process class to support worker processes.

--- src/lib/base/Process.ts
+++ src/lib/base/Process.ts
@@ -10,31 +10,19 @@ import {isAsyncFunction} from 'util/types'
 import {createServer, IncomingMessage, Server, ServerResponse} from 'http'
 import syncFetch from 'sync-fetch'
 import asyncFetch from 'node-fetch'
-import {GetPort} from '../../exports/Utilities'
+import {As, GetPort} from '../../exports/Utilities'
 import {format as URLFormat, parse as URLParse, UrlObject} from 'url'
 import {parse as QueryParse, ParsedUrlQuery} from 'querystring'
+import {AppendAsyncConstructor} from './async-constructor/Append'
+import {BaseObject} from './BaseObject'
 
 @Scoped(true)
 export class Process extends Component {
 
-    protected declare __$$worker: ChildProcess
-
-    protected declare __$$CServer: Server
-
-    /**
-     * 判断当前是否在主进程
-     * @protected
-     */
-    protected readonly isPrimary: boolean = !process.env.isWorkerProcess
-
-    /**
-     * 判断当前是否在子进程
-     * @protected
-     */
-    protected readonly isWorker: boolean = !!process.env.isWorkerProcess
-
     @Configurable()
-    public readonly concurrent: number = 1
+    public readonly concurrent: number
+
+    protected testProp: string
 
     /**
      * Constructor
@@ -43,38 +31,47 @@ export class Process extends Component {
     constructor(properties: InjectionProperties = {}) {
         super(properties)
         this.setInternalProperty('type', 'Process')
-        if (this.isPrimary) {
-            const publicMethods: string[] = Object.getOwnPropertyNames(this.constructor.prototype)
-                .filter((name: string) => typeof this.constructor.prototype[name] === 'function' && name !== 'constructor')
-            publicMethods.forEach((publicMethod: string) => {
-                const originMethod: Function = this[publicMethod]
-                const isAsyncMethod: boolean = isAsyncFunction(originMethod)
-                Object.defineProperty(this, publicMethod, {
-                    get(): any {
-                        const workerUrlObject: UrlObject = {
-                            protocol: 'http',
-                            hostname: 'localhost',
-                            port: Reflect.getOwnMetadata(this.__$$worker, this.__$$worker)
-                        }
-                        return isAsyncMethod ? async (...args: any[]): Promise<any> => {
-                            workerUrlObject.query = {
-                                method: publicMethod,
-                                args: v8.serialize(args).toString('base64')
+        AppendAsyncConstructor(this, async (): Promise<void> => {
+            if (!this.isWorker()) {
+                const publicMethods: string[] = Object.getOwnPropertyNames(this.constructor.prototype).filter((name: string) => typeof this.constructor.prototype[name] === 'function' && name !== 'constructor')
+                publicMethods.forEach((publicMethod: string) => {
+                    const originMethod: Function = this[publicMethod]
+                    const isAsyncMethod: boolean = isAsyncFunction(originMethod)
+                    Object.defineProperty(this, publicMethod, {
+                        get(): any {
+                            const workerUrlObject: UrlObject = {
+                                protocol: 'http',
+                                hostname: 'localhost',
+                                port: this.getInternalProperty('workerCommunicationPort')
                             }
-                            const rawResponse: string = await (await asyncFetch(URLFormat(workerUrlObject))).text()
-                            return v8.deserialize(Buffer.from(rawResponse, 'base64'))
-                        } : (...args: any[]): any => {
-                            workerUrlObject.query = {
-                                method: publicMethod,
-                                args: v8.serialize(args).toString('base64')
+                            return isAsyncMethod ? async (...args: any[]): Promise<any> => {
+                                workerUrlObject.query = {
+                                    method: publicMethod,
+                                    args: v8.serialize(args).toString('base64')
+                                }
+                                const rawResponse: string = await (await asyncFetch(URLFormat(workerUrlObject))).text()
+                                return v8.deserialize(Buffer.from(rawResponse, 'base64'))
+                            } : (...args: any[]): any => {
+                                workerUrlObject.query = {
+                                    method: publicMethod,
+                                    args: v8.serialize(args).toString('base64')
+                                }
+                                const rawResponse: string = syncFetch(URLFormat(workerUrlObject)).text()
+                                return v8.deserialize(Buffer.from(rawResponse, 'base64'))
                             }
-                            const rawResponse: string = syncFetch(URLFormat(workerUrlObject)).text()
-                            return v8.deserialize(Buffer.from(rawResponse, 'base64'))
                         }
-                    }
+                    })
                 })
-            })
-        }
+                const properties: string[] = this.propertyNames().filter((propertyName: string) => {
+                    return !publicMethods.includes(propertyName)
+                        && !(typeof this[propertyName] === 'function')
+                        && !(this[propertyName] instanceof BaseObject)
+                })
+                properties.forEach((property: string) => {
+                    console.log(property, Object.getOwnPropertyDescriptor(this, property))
+                })
+            }
+        })
         return this
     }
 
@@ -100,17 +97,25 @@ export class Process extends Component {
     }
 
     /**
+     * 判断是否为子进程
+     * @protected
+     */
+    protected isWorker(): boolean {
+        return !!process.env.isWorkerProcess
+    }
+
+    /**
      * 建立Worker子进程
      * @protected
      */
-    protected async __setupWorker(): Promise<void> {
+    protected async __setupWorkerProcess(): Promise<void> {
         const moduleId: string = this.__resolveSelfModuleId()
         const configurableProperties: string[] = await this.__getConfigurableProperties()
         const configs: Record<string, any> = {}
         configurableProperties.forEach((propertyKey: string) => configs[propertyKey] = this[propertyKey])
         const workerCommunicationPort: number = await GetPort()
         await new Promise((resolve, reject) => {
-            this.__$$worker = fork(path.resolve(__dirname, '../ProcessContainer'), [
+            this.setInternalProperty('worker', fork(path.resolve(__dirname, '../ProcessContainer'), [
                 moduleId,
                 this.className,
                 v8.serialize(configs).toString('base64'),
@@ -124,8 +129,30 @@ export class Process extends Component {
                 const eventName: string = args[0]
                 const eventArgs: any[] = args.slice(1)
                 this.emit(eventName, ...eventArgs)
-            }).on('error', reject)
-            Reflect.defineMetadata(this.__$$worker, workerCommunicationPort, this.__$$worker)
+            }).on('error', reject))
+            this.setInternalProperty('workerCommunicationPort', workerCommunicationPort)
+        })
+    }
+
+    /**
+     * 在子进程中执行进程初始化工作
+     * @protected
+     */
+    protected async __initWorkerProcess(): Promise<void> {
+        await new Promise<void>((resolve, reject) => {
+            try {
+                const workerCommunicationPortStr: string = process.argv[0]
+                this.setInternalProperty('CServer', createServer(async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+                    const parsedQuery: ParsedUrlQuery = QueryParse(URLParse(req.url!).query!)
+                    const method: string = <string>(parsedQuery.method)
+                    const args: any[] = v8.deserialize(Buffer.from(<string>(parsedQuery.args), 'base64'))
+                    const returnValue: any = await this[method](...args)
+                    res.write(v8.serialize(returnValue).toString('base64'))
+                    res.end()
+                }).listen(parseInt(workerCommunicationPortStr), 'localhost', resolve))
+            } catch (e) {
+                return reject(e)
+            }
         })
     }
 
@@ -134,29 +161,17 @@ export class Process extends Component {
      * @protected
      */
     protected async __init(): Promise<void> {
-        if (this.isPrimary) {
-            await this.__setupWorker()
-        } else {
-            await new Promise<void>((resolve, reject) => {
-                try {
-                    const workerCommunicationPortStr: string = process.argv[0]
-                    this.__$$CServer = createServer(async (req: IncomingMessage, res: ServerResponse) => {
-                        const parsedQuery: ParsedUrlQuery = QueryParse(URLParse(req.url!).query!)
-                        const method: string = <string>(parsedQuery.method)
-                        const args: any[] = v8.deserialize(Buffer.from(<string>(parsedQuery.args), 'base64'))
-                        const returnValue: any = await this[method](...args)
-                        res.write(v8.serialize(returnValue).toString('base64'))
-                        res.end()
-                    }).listen(parseInt(workerCommunicationPortStr), 'localhost', resolve)
-                } catch (e) {
-                    return reject(e)
-                }
-            })
-        }
+        await super.__init()
+        this.isWorker() ? await this.__initWorkerProcess() : await this.__setupWorkerProcess()
     }
 
+    /**
+     * 内部销毁函数
+     * @protected
+     */
     protected async __destroy(): Promise<void> {
-        //todo 销毁进程
+        if (this.hasInternalProperty('CServer')) await new Promise<void>(resolve => As<Server>(this.getInternalProperty('CServer')).close(() => resolve()))
+        if (this.hasInternalProperty('worker')) As<ChildProcess>(this.getInternalProperty('worker')).kill()
         return super.__destroy()
     }
 }
