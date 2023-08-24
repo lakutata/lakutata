3420f17e6ae06dadd7393643cc25865289060d2f
myq1991 <myq1991@gmail.com>
2023-08-24 16:05:47 +0800

    Refactor .prepublish.js to use fs/promises for file operations.

--- .prepublish.js
+++ .prepublish.js
@@ -1,11 +1,11 @@
 (async () => {
-    const {readFile, writeFile} = require('fs/promises')
+    const fsPromise = require('fs/promises')
 
     const target = 'package.json'
     const backup = '.package.json.bak'
 
-    const originPkgJson = JSON.parse(await readFile(target, {encoding: 'utf-8'}))
-    await writeFile(backup, JSON.stringify(originPkgJson), {encoding: 'utf-8'})
+    const originPkgJson = JSON.parse(await fsPromise.readFile(target, {encoding: 'utf-8'}))
+    await fsPromise.writeFile(backup, JSON.stringify(originPkgJson), {encoding: 'utf-8'})
 
     delete originPkgJson['release-it']
     delete originPkgJson['devDependencies']
@@ -13,5 +13,5 @@
     for (const scriptName of scriptNames) {
         if (/release|test/.test(scriptName)) delete originPkgJson.scripts[scriptName]
     }
-    await writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
+    await fsPromise.writeFile(target, JSON.stringify(originPkgJson, null, 2), {encoding: 'utf-8', flag: 'w+'})
 })()
