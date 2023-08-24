9e0ce621ab735f0a9ec3e31ed9d43f5b5f22fa8c
myq1991 <myq1991@gmail.com>
2023-08-24 16:03:03 +0800

    Refactor code in .postpublish.js to use the fs/promises module and os.EOL.

--- .postpublish.js
+++ .postpublish.js
@@ -1,11 +1,14 @@
 (async () => {
-    const {readFile, writeFile, rm} = require('fs/promises')
-    const {EOL} = require('os')
+    const fsPromise = require('fs/promises')
+    const os = require('os')
 
     const target = 'package.json'
     const backup = '.package.json.bak'
 
-    const originPkgJson = JSON.parse(await readFile(backup, {encoding: 'utf-8'}))
-    await writeFile(target, `${JSON.stringify(originPkgJson, null, 2)}${EOL}`, {encoding: 'utf-8', flag: 'w+'})
-    await rm(backup, {force: true, recursive: true})
+    const originPkgJson = JSON.parse(await fsPromise.readFile(backup, {encoding: 'utf-8'}))
+    await fsPromise.writeFile(target, `${JSON.stringify(originPkgJson, null, 2)}${os.EOL}`, {
+        encoding: 'utf-8',
+        flag: 'w+'
+    })
+    await fsPromise.rm(backup, {force: true, recursive: true})
 })()
