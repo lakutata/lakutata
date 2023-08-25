4cb5b0ba7bfd92460896ed467edc8691f0ab5167
myq1991 <myq1991@gmail.com>
2023-08-25 20:27:06 +0800

    chore: release v0.1.2-proto.3

--- src/tests/threads/TestThreadTask.ts
+++ src/tests/threads/TestThreadTask.ts
@@ -3,6 +3,6 @@ import {ThreadTask} from '../../lib/base/abstracts/ThreadTask'
 export class TestThreadTask extends ThreadTask {
     protected async executor(workData: any): Promise<any> {
         this.log.info('this is message from sub thread!!!!!!!!!!!!!!!!')
-        return 'g<><><><><><><><><><><><><><><><><><><>g' + workData
+        return 'g<><><><><><><><><><><><><><><><><><><><><><><><><>g' + workData
     }
 }
