8c28840cfaa63edc19271f4880057fa587f88dcd
myq1991 <myq1991@gmail.com>
2023-09-04 11:55:19 +0800

    Remove files

--- src/tests/intervals/TestCron.ts
+++
@@ -1,7 +0,0 @@
-import {Cron} from '../../lib/base/abstracts/Cron'
-
-export class TestCron extends Cron {
-    protected async executor(): Promise<void> {
-        console.log('this is cron executor')
-    }
-}
