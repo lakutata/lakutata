42d5b2d293654c2b31ae3c704d6cea7b9c313ce5
myq1991 <myq1991@gmail.com>
2023-09-19 14:33:36 +0800

    Refactor renderChunk function to only include ts-node registration for specific file types.

--- tsup.config.cjs.ts
+++ tsup.config.cjs.ts
@@ -34,7 +34,7 @@ export default defineConfig(() => {
         plugins: [{
             name: 'ModifyDistFiles',
             renderChunk: (code, chunkInfo) => {
-                if (chunkInfo.path.endsWith('ProcessContainer.js') || chunkInfo.path.endsWith('ThreadContainer.js')|| chunkInfo.path.endsWith('worker.js')) {
+                if (chunkInfo.path.endsWith('ProcessContainer.js') || chunkInfo.path.endsWith('ThreadContainer.js')) {
                     code = `\nif (require.resolve('ts-node')) require('ts-node').register()\n${code}`
                 }
                 return {
