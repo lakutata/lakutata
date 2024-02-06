e8c339cd739dbe374dadca4d34e66c16c8e6181a
myq1991 <myq1991@gmail.com>
2024-02-06 17:15:53 +0800

    Update rollup.config.js: added rollup-plugin-copy dependency.

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,21 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import swc from '@rollup/plugin-swc'
+import copy from 'rollup-plugin-copy'
 import path from 'node:path'
 import {sync as globFiles} from 'glob'
 
 const normalizeString = (str) => Buffer.from(str).filter((v, i) => i ? true : v !== 0).toString()
 const currentWorkingDir = normalizeString(process.cwd())
 const thirdPartyPackageRootDirname = 'modules'
+const outputDirname = 'distro'
+const jsrcOutputDirname = path.join(outputDirname, 'src')
 
 export default {
     input: globFiles('src/**/*.ts'),
     output: {
         format: 'esm',
-        dir: 'distro',
+        dir: outputDirname,
         exports: 'named',
         compact: false,//减小文件体积
         manualChunks: (id) => {
@@ -55,13 +58,19 @@ export default {
         resolve(),
         commonjs(),
         typescript({
-            outDir: 'distro/src',
+            outDir: jsrcOutputDirname,
             esModuleInterop: true,
             isolatedModules: true,
             declaration: true,
             emitDecoratorMetadata: true,
             declarationMap: true
         }),
-        swc()
+        swc(),
+        copy({
+            targets: [
+                {src: 'src/cpp/**/*', dest: path.join(jsrcOutputDirname, 'cpp')},
+                {src: 'binding.gyp', dest: outputDirname}
+            ]
+        })
     ]
 }
