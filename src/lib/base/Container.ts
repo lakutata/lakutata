620938c2286e23ad85f6e6912279bb5b6fe79e1a
myq1991 <myq1991@gmail.com>
2024-02-22 23:44:40 +0800

    "Add DevNull.ts and modify Container.ts to include createScope() and destroy()"

--- src/lib/base/Container.ts
+++ src/lib/base/Container.ts
@@ -1,3 +1,24 @@
+import {createContainer, IDependencyInjectionContainer} from '../ioc/DependencyInjectionContainer.js'
+
 export class Container {
-    //TODO
+
+    #dic: IDependencyInjectionContainer
+
+    constructor(parent?: Container) {
+        this.#dic = parent ? parent.#dic.createScope() : createContainer({injectionMode: 'PROXY', strict: true})
+    }
+
+    /**
+     * Create sub container scope
+     */
+    public createScope(): Container {
+        return new Container(this)
+    }
+
+    /**
+     * Destroy current container
+     */
+    public async destroy(): Promise<void> {
+        await this.#dic.dispose()
+    }
 }
---
+++ src/lib/base/func/DevNull.ts
@@ -0,0 +1,9 @@
+/**
+ * Just accept arguments
+ * @param args
+ * @constructor
+ */
+export function DevNull(...args: any[]): void {
+    //Do nothing
+    return void (0)
+}
