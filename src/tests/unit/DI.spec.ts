8632a9a300b30ff50eee252e83e075a4ebaddb95
myq1991 <myq1991@gmail.com>
2024-02-29 11:50:06 +0800

    Add registration tests for named and unnamed classes in DI.spec.ts.

--- src/tests/unit/DI.spec.ts
+++ src/tests/unit/DI.spec.ts
@@ -1,10 +1,29 @@
 import {describe, it} from 'node:test'
+import {Container} from '../../lib/core/Container.js'
+import {BaseObject} from '../../lib/base/BaseObject.js'
+import assert from 'node:assert'
+
+class RegistrationTestClass extends BaseObject {
+    public foo(): string {
+        return 'bar'
+    }
+}
 
 describe('DI Test', async function (): Promise<void> {
-    await it('should 1', async (context): Promise<void> => {
-        console.log('1')
+    const container: Container = new Container()
+
+    await it('add named(symbol) registration', async (): Promise<void> => {
+        const symbol: symbol = Symbol('test')
+        await container.load({[symbol]: RegistrationTestClass})
+        const registration: RegistrationTestClass = await container.get<RegistrationTestClass>(symbol)
+        assert.equal(registration.foo(), 'bar')
     })
-    await it('should 2', async (context): Promise<void> => {
-        console.log('2')
+    await it('add named(string) registration', async (): Promise<void> => {
+        await container.load({test: RegistrationTestClass})
+        const registration: RegistrationTestClass = await container.get<RegistrationTestClass>('test')
+        assert.equal(registration.foo(), 'bar')
+    })
+    await it('add unnamed(constructor) registration', async (): Promise<void> => {
+        //TODO
     })
 })
