1d04536823eb4f25d0d4e8022554b727133dd2fc
myq1991 <myq1991@gmail.com>
2024-03-05 20:50:16 +0800

    Add unit tests for DTO class and import DTO.spec.js in UnitTest.spec.ts.

--- src/tests/UnitTest.spec.ts
+++ src/tests/UnitTest.spec.ts
@@ -3,4 +3,5 @@ import {describe} from 'node:test'
 await describe('Lakutata Unit Test', async function (): Promise<void> {
     await import('./unit/DI.spec.js')
     await import('./unit/BaseObject.spec.js')
+    await import('./unit/DTO.spec.js')
 })
