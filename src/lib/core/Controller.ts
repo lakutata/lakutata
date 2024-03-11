3f57d58d61d45c523e2a28dd0bfa00281223e4f2
myq1991 <myq1991@gmail.com>
2024-03-12 01:29:51 +0800

    Add new file Controller.ts to src/lib/core directory
    This commit adds a new file, Controller.ts, to the src/lib/core directory. The file defines a base class for controllers and extends the Provider class. The Controller class is also annotated with the @Scoped(true) decorator, indicating that it has a scoped lifetime. Additionally, the class has a static property $objectType of type ObjectType, which is set to ObjectType.Controller.

---
+++ src/lib/core/Controller.ts
@@ -0,0 +1,13 @@
+import {Provider} from './Provider.js'
+import {ObjectType} from '../base/internal/ObjectType.js'
+import {Scoped} from '../../decorators/di/Lifetime.js'
+
+/**
+ * Controller base class
+ */
+@Scoped(true)
+export class Controller extends Provider {
+
+    public static accessor $objectType: ObjectType = ObjectType.Controller
+
+}
