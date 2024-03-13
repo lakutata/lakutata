21a60b64c17b5e7de2f6dfba7d3e1654b9ca25b1
myq1991 <myq1991@gmail.com>
2024-03-13 21:58:57 +0800

    Rename IPCAction.ts to ServiceAction.ts in the src/decorators/ctrl directory.

--- src/decorators/ctrl/IPCAction.ts
+++
@@ -1,8 +0,0 @@
-import {Controller} from '../../lib/core/Controller.js'
-import {TMethodDecorator} from '../../types/TMethodDecorator.js'
-
-export function IPCAction<ClassPrototype extends Controller, Method>(): TMethodDecorator<ClassPrototype, Method> {
-    return (target: ClassPrototype, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<Method>) => {
-        //TODO
-    }
-}
