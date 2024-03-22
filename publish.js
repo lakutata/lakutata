0317a74f24399c434a98fbc344dfdb81ce0ea5d9
myq1991 <myq1991@gmail.com>
2024-03-22 16:48:30 +0800

    feat(publish): update publish script to compile before publishing
    - Update the publish script in package.json to run the compile command before publishing.
    - This ensures that the latest changes are compiled before publishing the package.

---
+++ publish.js
@@ -0,0 +1,39 @@
+import cliSpinners from 'cli-spinners'
+import logUpdate from 'log-update'
+import {execa} from 'execa'
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
+import {readFile} from 'node:fs/promises'
+import chalk from 'chalk'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const spinnerJob = async (description, job, successHandler, errorHandler) => {
+    let i = 0
+    const spinnerInterval = setInterval(() => {
+        const frames = cliSpinners.dots.frames
+        const text = `${frames[i = ++i % frames.length]} ${description}`
+        logUpdate(text)
+    }, cliSpinners.dots.interval)
+    try {
+        await job()
+        clearInterval(spinnerInterval)
+        logUpdate.clear()
+        return successHandler()
+    } catch (e) {
+        clearInterval(spinnerInterval)
+        logUpdate.clear()
+        return errorHandler(e)
+    }
+}
+const distroDir = path.resolve(__dirname, 'distro')
+const distroPackageJson = JSON.parse(await readFile(path.resolve(distroDir, 'package.json'), {encoding: 'utf-8'}))
+process.exit(await spinnerJob('Publishing', async () => {
+    await execa('npm', ['publish'], {cwd: distroDir})
+}, () => {
+    console.info(chalk.green(`Version ${distroPackageJson.version} has been successfully published`))
+    return 0
+}, (e) => {
+    console.info(`${chalk.red('The publication encountered an error and failed to be published:')}\n${chalk.red(e.message)}`)
+    return 1
+}))
