c62f58fe7d6f9e6ba03beba7ac00a8f5767be9c1
myq1991 <myq1991@gmail.com>
2024-03-25 22:32:59 +0800

    feat: Add unit tests for DI, BaseObject, and DTO classes

--- src/tests/UnitTest.spec.ts
+++ src/tests/UnitTest.spec.ts
@@ -1,7 +1,12 @@
 import {describe} from 'node:test'
+import DITests from './unit/DI.spec.js'
+import BaseObjectTests from './unit/BaseObject.spec.js'
+import DTOTests from './unit/DTO.spec.js'
 
-await describe('Lakutata Unit Test', async function (): Promise<void> {
-    await import('./unit/DI.spec.js')
-    await import('./unit/BaseObject.spec.js')
-    await import('./unit/DTO.spec.js')
-})
+(async () => {
+    await describe('Lakutata Unit Test', async function (): Promise<void> {
+        await DITests()
+        await BaseObjectTests()
+        await DTOTests()
+    })
+})()
--- src/tests/unit/DI.spec.ts
+++ src/tests/unit/DI.spec.ts
@@ -23,71 +23,73 @@ class RegistrationTestClass extends BaseObject {
     }
 }
 
-await describe('DI Test', async function (): Promise<void> {
-    const container: Container = new Container()
+export default async function (){
+    await describe('DI Test', async function (): Promise<void> {
+        const container: Container = new Container()
 
-    await it('add named(symbol) registration', async (): Promise<void> => {
-        const symbol: symbol = Symbol('test')
-        await container.load([{id: symbol, class: RegistrationTestClass}])
-        const registration: RegistrationTestClass = await container.get<RegistrationTestClass>(symbol)
-        assert.equal(registration.foo(), 'bar')
-    })
-    await it('add named(string) registration', async (): Promise<void> => {
-        await container.load([{id: 'test', class: RegistrationTestClass}])
-        const registration: RegistrationTestClass = await container.get<RegistrationTestClass>('test')
-        assert.equal(registration.foo(), 'bar')
-    })
-    await it('get autoload registration', async (): Promise<void> => {
-        @Autoload()
-        class AutoloadRegistrationTestClass extends RegistrationTestClass {
-            public foo(): string {
-                return 'autoload'
-            }
-        }
-
-        const registration: AutoloadRegistrationTestClass = await container.get<AutoloadRegistrationTestClass>(AutoloadRegistrationTestClass)
-        assert.equal(registration.foo(), 'autoload')
-    })
-    await it('load module by glob', async (): Promise<void> => {
-        await container.load([`${path.resolve(__dirname, './resources/glob-modules')}/**.js`])
-        const registration: TestObj = await container.get(TestObj)
-        assert.equal(registration.foo(), 'bar')
-    })
-    await it('builds an instance of a base object class by injecting dependencies, but without registering it in the container', async (): Promise<void> => {
-        class TmpObject extends BaseObject {
-            public foo(): string {
-                return 'tmp'
-            }
-        }
-
-        const registration: TmpObject = await container.build(TmpObject)
-        assert.equal(registration.foo(), 'tmp')
-    })
-    const subContainer: Container = container.createScope()
-    assert.notEqual(subContainer, container)
-    await it('get root container registration in sub container', async (): Promise<void> => {
-        const registration: RegistrationTestClass = await subContainer.get<RegistrationTestClass>('test')
-        assert.equal(registration.foo(), 'bar')
-    })
-    await it('add registration to sub container', async (): Promise<void> => {
-        await subContainer.load([{
-            id: 'subTest',
-            class: class extends RegistrationTestClass {
+        await it('add named(symbol) registration', async (): Promise<void> => {
+            const symbol: symbol = Symbol('test')
+            await container.load([{id: symbol, class: RegistrationTestClass}])
+            const registration: RegistrationTestClass = await container.get<RegistrationTestClass>(symbol)
+            assert.equal(registration.foo(), 'bar')
+        })
+        await it('add named(string) registration', async (): Promise<void> => {
+            await container.load([{id: 'test', class: RegistrationTestClass}])
+            const registration: RegistrationTestClass = await container.get<RegistrationTestClass>('test')
+            assert.equal(registration.foo(), 'bar')
+        })
+        await it('get autoload registration', async (): Promise<void> => {
+            @Autoload()
+            class AutoloadRegistrationTestClass extends RegistrationTestClass {
                 public foo(): string {
-                    return 'sub-bar'
+                    return 'autoload'
                 }
             }
-        }])
-        const registration: RegistrationTestClass = await subContainer.get<RegistrationTestClass>('subTest')
-        assert.equal(registration.foo(), 'sub-bar')
-    })
-    await it('root container should not allowed access registrations inside sub container', async (): Promise<void> => {
-        assert.equal(container.has('subTest'), false)
-    })
 
-    await it('all registration instance should be destroyed after root container destroyed', async (): Promise<void> => {
-        await container.destroy()
-        assert.equal(instanceSet.size, 0)
-    })
+            const registration: AutoloadRegistrationTestClass = await container.get<AutoloadRegistrationTestClass>(AutoloadRegistrationTestClass)
+            assert.equal(registration.foo(), 'autoload')
+        })
+        await it('load module by glob', async (): Promise<void> => {
+            await container.load([`${path.resolve(__dirname, './resources/glob-modules')}/**.js`])
+            const registration: TestObj = await container.get(TestObj)
+            assert.equal(registration.foo(), 'bar')
+        })
+        await it('builds an instance of a base object class by injecting dependencies, but without registering it in the container', async (): Promise<void> => {
+            class TmpObject extends BaseObject {
+                public foo(): string {
+                    return 'tmp'
+                }
+            }
 
-})
+            const registration: TmpObject = await container.build(TmpObject)
+            assert.equal(registration.foo(), 'tmp')
+        })
+        const subContainer: Container = container.createScope()
+        assert.notEqual(subContainer, container)
+        await it('get root container registration in sub container', async (): Promise<void> => {
+            const registration: RegistrationTestClass = await subContainer.get<RegistrationTestClass>('test')
+            assert.equal(registration.foo(), 'bar')
+        })
+        await it('add registration to sub container', async (): Promise<void> => {
+            await subContainer.load([{
+                id: 'subTest',
+                class: class extends RegistrationTestClass {
+                    public foo(): string {
+                        return 'sub-bar'
+                    }
+                }
+            }])
+            const registration: RegistrationTestClass = await subContainer.get<RegistrationTestClass>('subTest')
+            assert.equal(registration.foo(), 'sub-bar')
+        })
+        await it('root container should not allowed access registrations inside sub container', async (): Promise<void> => {
+            assert.equal(container.has('subTest'), false)
+        })
+
+        await it('all registration instance should be destroyed after root container destroyed', async (): Promise<void> => {
+            await container.destroy()
+            assert.equal(instanceSet.size, 0)
+        })
+
+    })
+}
