fc85cfa636befbf727c2413e4f48e607a51bced3
myq1991 <myq1991@gmail.com>
2024-03-26 22:31:37 +0800

    feat(Logger): Add error, warn, info, debug, and trace methods
    - Added error, warn, info, debug, and trace methods to the Logger class
    - These methods allow for writing logs at different levels (error, warn, info, debug, and trace)
    - Each method can accept different parameters (object, message, and additional arguments)
    - The configured log level determines whether the log is written or not

--- src/components/Logger.ts
+++ src/components/Logger.ts
@@ -32,36 +32,121 @@ export class Logger extends Component {
         }, As(PinoPretty)())
     }
 
+    /**
+     * Write a 'error' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public error<T extends object>(obj: T, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'error' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public error(obj: unknown, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'error' level log, if the configured level allows for it.
+     * @param msg
+     * @param args
+     */
     public error(msg: string, ...args: any[]): void
     public error(obj: any, ...msg: (any)[]): void {
         return this.#instance.error(obj, ...msg)
     }
 
+    /**
+     * Write a 'warn' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public warn<T extends object>(obj: T, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'warn' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public warn(obj: unknown, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'warn' level log, if the configured level allows for it.
+     * @param msg
+     * @param args
+     */
     public warn(msg: string, ...args: any[]): void
     public warn(obj: any, ...msg: (any)[]): void {
         return this.#instance.warn(obj, ...msg)
     }
 
+    /**
+     * Write a 'info' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public info<T extends object>(obj: T, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'info' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public info(obj: unknown, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'info' level log, if the configured level allows for it.
+     * @param msg
+     * @param args
+     */
     public info(msg: string, ...args: any[]): void
     public info(obj: any, ...msg: (any)[]): void {
         return this.#instance.info(obj, ...msg)
     }
 
+    /**
+     * Write a 'debug' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public debug<T extends object>(obj: T, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'debug' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public debug(obj: unknown, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'debug' level log, if the configured level allows for it.
+     * @param msg
+     * @param args
+     */
     public debug(msg: string, ...args: any[]): void
     public debug(obj: any, ...msg: (any)[]): void {
         return this.#instance.debug(obj, ...msg)
     }
 
+    /**
+     * Write a 'trace' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public trace<T extends object>(obj: T, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'trace' level log, if the configured level allows for it.
+     * @param obj
+     * @param msg
+     * @param args
+     */
     public trace(obj: unknown, msg?: string, ...args: any[]): void
+    /**
+     * Write a 'trace' level log, if the configured level allows for it.
+     * @param msg
+     * @param args
+     */
     public trace(msg: string, ...args: any[]): void
     public trace(obj: any, ...msg: (any)[]): void {
         return this.#instance.trace(obj, ...msg)
