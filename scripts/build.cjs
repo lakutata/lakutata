2962dbf3532dd9ced5ad3bea07565b97c2a5dc7d
myq1991 <myq1991@gmail.com>
2024-03-30 17:11:55 +0800

    feat: Rename buildAll.cjs to build.cjs and build-ffi.js to build-ffi.cjs

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,5 +1,8 @@
+/**
+ * Build all cpp binary
+ */
 (async () => {
     const path = require('node:path')
     const vendorDir = path.resolve(__dirname, '../vendor')
-    await require('./build-ffi.js')(vendorDir)
+    await require('./build-ffi.cjs')(vendorDir)
 })()
