e1fc50b45e5c4720c4c3fd5454a76005b86336b9
myq1991 <myq1991@gmail.com>
2025-07-14 11:06:53 +0800

    refactor(lib/helpers): replace bignumber.js with bn.js

--- src/lib/helpers/BigNumber.ts
+++ src/lib/helpers/BigNumber.ts
@@ -1,19 +1,3 @@
-import BigNumberJs from 'bignumber.js'
+import BigNumber from 'bn.js'
 
-export type BigNumberValue = BigNumberJs.Value
-export type BigNumberConfig = BigNumberJs.Config
-export type BigNumberFormat = BigNumberJs.Format
-export type BigNumberInstance = BigNumberJs.Instance
-export type BigNumberConstructor = BigNumberJs.Constructor
-export type BigNumberModuloMode = BigNumberJs.ModuloMode
-export type BigNumberRoundingMode = BigNumberJs.RoundingMode
-
-export class BigNumber extends BigNumberJs {
-    constructor(n: BigNumberValue, base?: number) {
-        super(n, base)
-    }
-
-    public abs(): BigNumber {
-        return super.abs()
-    }
-}
+export {BigNumber}
