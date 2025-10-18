7aabb55834c6e77d566e20c9c2aecff1ff4c35ff
myq1991 <myq1991@gmail.com>
2025-10-19 00:04:22 +0800

    feat(helpers): refactor Paginator to return standardized pagination result

--- src/lib/helpers/Paginator.ts
+++ src/lib/helpers/Paginator.ts
@@ -1,12 +1,26 @@
 import {PaginationSearchDTO} from '../../dto/PaginationSearchDTO.js'
 import {PaginationResultDTO} from '../../dto/PaginationResultDTO.js'
 
+export interface PaginatorHandlerResult<T> {
+    total: number
+    items: T[]
+}
+
 /**
  * Paginator
  * @param options
  * @param handler
  * @constructor
  */
-export async function Paginator<T, Options extends PaginationSearchDTO, Result extends PaginationResultDTO<T>>(options: Options, handler: (options: Options, limit: number, offset: number) => Promise<Result>): Promise<Result> {
-    return await handler(options, options.limit!, options.offset!)
+export async function Paginator<T, Options extends PaginationSearchDTO>(options: Options, handler: (options: Options, limit: number, offset: number) => Promise<PaginatorHandlerResult<T>>): Promise<PaginationResultDTO<T>> {
+    const result: PaginatorHandlerResult<T> = await handler(options, options.limit!, options.offset!)
+    return {
+        items: result.items,
+        meta: {
+            count: result.items.length,
+            total: result.total,
+            limit: options.limit!,
+            offset: options.offset!
+        }
+    }
 }
